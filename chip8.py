22d8042cbb86e7553ea7b43025f4ef5abd07d2be
Spucky <126801350+Spucky1@users.noreply.github.com>
2024-08-22 13:31:02 -0500

    Statated oop

---
+++ chip8.py
@@ -0,0 +1,23 @@
+
+
+
+class chip8:
+    def __init__(self):
+        self.registers = [16]
+        self.memory = [4096]
+        self.index
+        self.pc
+        self.stack = [16]
+        self.sp 
+        self.delaytimer
+        self.soundtimer
+        self.keypad = [16]
+        self.video[64*32]
+        self.opcode
+
+    
+    
+
+
+
+
--- main.c
+++
@@ -1,5 +0,0 @@
-#include <stidio.h>
- int main(){
-    printf("hello wrld");
-    return 0;
- }
