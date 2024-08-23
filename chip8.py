038c60a929e9493b4089435acef5f20b5b21dc10
Spucky <126801350+Spucky1@users.noreply.github.com>
2024-08-23 12:11:28 -0500

    Update chip8.py

--- chip8.py
+++ chip8.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 class chip8:
@@ -14,6 +14,22 @@ class chip8:
         self.keypad = [16]
         self.video[64*32]
         self.opcode
+rom = False
+
+Start_Address = 0x200
+
+def chip8__Loadrom(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+            rom = True
+if rom == True:
+    os.open()
+
+
+
+
+
 
     
     
