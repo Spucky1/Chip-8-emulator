7d7f663a371a218dd98ee1ace3c55e551caddce8
Spucky <126801350+Spucky1@users.noreply.github.com>
2024-09-16 11:29:25 -0500

    Update chip8.py

--- chip8.py
+++ chip8.py
@@ -1,5 +1,6 @@
 import os
 from os import path
+import binascii
 print("script is running")
 
 
@@ -16,20 +17,24 @@ class chip8:
         self.keypad = [16]
         self.video[64*32]
         self.opcode
+
+
     
 START_ADDRES = int(0x200)
 def findrom_chip8(rom_name, path):
         for root, dirs, files, in os.walk(path):
             if rom_name in files:
                 os.path.join(root, rom_name)
-                with open(rom_name, "r")as r:
-                     contents = os.path.getsize(path)
-                     print(contents)
+                with open(rom_name, "rb")as r:
+                    hexdata = binascii.hexlify(r.read())
+                    print(hexdata)
+                    
+                    
 def loadrom_chip8():
      print("room is loading")
 def main():
      findrom_chip8("Airplane.ch8","./")
-     loadrom_chip8()
+    
 main()
      
 
